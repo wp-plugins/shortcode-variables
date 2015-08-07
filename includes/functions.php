1ce44ed10831cf96f3df552d55e866f24927799d
aliakro <aliakro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-07 14:08:44 +0000

    1.3.1 Release
    git-svn-id: https://plugins.svn.wordpress.org/shortcode-variables/tags/1.3.1@1214901 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/functions.php
@@ -0,0 +1,313 @@
+<?php
+
+defined('ABSPATH') or die('Jog on!');
+
+function sh_cd_get_all_shortcodes()
+{
+    global $wpdb;
+
+    $sql = 'SELECT * FROM ' . $wpdb->prefix . SH_CD_TABLE . ' order by slug asc';
+
+    $rows = $wpdb->get_results( $sql );
+
+    if (!is_null($rows))
+        return $rows;
+    
+    return false;
+    
+}
+
+function sh_cd_get_shortcode($id)
+{
+    if (!is_admin())
+        return false;
+
+    global $wpdb;
+
+    $sql = $wpdb->prepare('SELECT slug, data FROM ' . $wpdb->prefix . SH_CD_TABLE . ' where id = %d', $id);
+
+    $row = $wpdb->get_row( $sql );
+
+    if (!is_null($row))
+        return $row;
+    
+    return false;
+    
+}
+function sh_cd_get_shortcode_by_slug($slug)
+{
+    global $wpdb;
+
+    $sql = $wpdb->prepare('SELECT data FROM ' . $wpdb->prefix . SH_CD_TABLE . ' where slug = %s', $slug);
+
+    $row = $wpdb->get_var( $sql );
+
+    if (!is_null($row))
+        return stripslashes($row);
+    
+    return false;
+    
+}
+function sh_cd_get_slug_by_id($id)
+{
+    global $wpdb;
+
+    $sql = $wpdb->prepare('SELECT slug FROM ' . $wpdb->prefix . SH_CD_TABLE . ' where id = %d', $id);
+
+    $row = $wpdb->get_var( $sql );
+
+    if (!is_null($row))
+        return $row;
+    
+    return false;
+    
+}
+
+function sh_cd_save_shortcode($slug, $data, $id = false)
+{
+    if (!is_admin())
+        return false;
+
+    if (false == $id && empty($slug))
+        return false;
+
+    global $wpdb;
+
+    $slug = sh_cd_get_slug($slug);
+
+    $result = false;
+
+    if ($id)
+    {
+        //Update data (Slug can never be updated!)
+        $result = $wpdb->update( 
+            $wpdb->prefix . SH_CD_TABLE, 
+            array( 
+                'data' => $data 
+            ), 
+            array( 'id' => $id), 
+            array( 
+                '%s'   
+            ), 
+            array( '%d' ) 
+        );
+
+        sh_cd_delete_cache(sh_cd_get_slug_by_id($id));
+    }
+    else
+    {
+        $result = $wpdb->insert( 
+            $wpdb->prefix . SH_CD_TABLE,
+            array( 
+                'slug' => $slug, 
+                'data' => $data 
+            ), 
+            array( 
+                '%s',   
+                '%s'   
+            )
+        );
+
+        sh_cd_delete_cache($slug);
+    }     
+
+    if (false === $result) {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+    
+}
+
+function sh_cd_delete_shortcode($id)
+{
+    if (!is_admin() || !is_numeric($id))
+        return false;
+
+    global $wpdb;
+
+    if (false === $wpdb->delete( $wpdb->prefix . SH_CD_TABLE, array( 'id' => $id ) )) {
+        return false; 
+    } 
+    else {
+        return true;
+    }
+   
+}
+
+function sh_cd_get_slug($text)
+{
+    if(!empty($text))
+    {
+        $text = sanitize_title($text);
+
+        $original_slug = $text;
+
+        $try = 1; 
+        var_dump(in_array($text, sh_cd_shortcode_presets()));
+
+        // If slug exists, then fetch a unique one!
+        //  v1.1: and ensure slug isn't a preset
+        while (!sh_cd_is_slug_unique($text))
+        {
+            $text = $original_slug . '_' . $try;
+           
+            $try++; 
+        }
+    }
+
+    return $text;
+}
+
+function sh_cd_is_slug_unique($slug)
+{
+    if (!is_admin() || empty($slug))
+        return false;
+
+    // 1.1 Ensure slug is not a prefix
+    if (sh_cd_is_shortcode_preset($slug))
+        return false;
+
+    global $wpdb;
+
+    $sql = $wpdb->prepare('SELECT count(slug) FROM ' . $wpdb->prefix . SH_CD_TABLE . ' where slug = %s', $slug);
+
+    $row = $wpdb->get_var( $sql );
+
+    if(0 == $row)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+
+}
+
+function sh_cd_create_database_table()
+{
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . SH_CD_TABLE;
+	
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+	  id mediumint(9) NOT NULL AUTO_INCREMENT,
+	  slug varchar(100) NOT NULL,
+	  data text,
+	  UNIQUE KEY id (id)
+	) $charset_collate;";
+
+    $wpdb->query($sql);
+
+    update_option('sh-cd-version', SH_CD_PLUGIN_VERSION);
+
+}
+
+function sh_cd_display_message($text, $error = false)
+{
+    if (!empty($text))
+    {
+        $class = (($error) ? 'error' : 'updated');
+
+        echo '<div class="' . $class . '">
+                <p>' . $text . '</p>
+            </div>';
+    }
+}
+
+function sh_cd_create_dialog_jquery_code($title, $message, $class_used_to_prompt_confirmation, $js_call = false)
+{
+	global $wp_scripts;
+	$queryui = $wp_scripts->query('jquery-ui-core');
+	$url = "//ajax.googleapis.com/ajax/libs/jqueryui/".$queryui->ver."/themes/smoothness/jquery-ui.css";
+	wp_enqueue_script( 'jquery-ui-dialog' );
+	wp_enqueue_style('jquery-ui-smoothness', $url, false, null);
+
+    $id_hash = md5($title . $message . $class_used_to_prompt_confirmation);
+
+    ?>
+    <div id='<?php echo $id_hash; ?>' title='<?php echo $title; ?>'>
+      <p><?php echo $message; ?></p>
+    </div>
+     <script>
+          jQuery(function($) {
+           
+            var $info = $('#<?php echo $id_hash; ?>');
+            
+            $info.dialog({                   
+                'dialogClass'   : 'wp-dialog',           
+                'modal'         : true,
+                'autoOpen'      : false
+            });
+           
+            $('.<?php echo $class_used_to_prompt_confirmation; ?>').click(function(event) {
+                
+                event.preventDefault();
+
+                target_url = $(this).attr('href');
+             
+                var $info = $('#<?php echo $id_hash; ?>');
+               
+                $info.dialog({                   
+                    'dialogClass'   : 'wp-dialog',           
+                    'modal'         : true,
+                    'autoOpen'      : false, 
+                    'closeOnEscape' : true,      
+                    'buttons'       : {
+                        'Yes': function() {
+
+                            <?php if ($js_call != false): ?>
+                                <?php echo $js_call; ?>
+
+                                $(this).dialog('close');
+                            <?php else: ?>
+                                window.location.href = target_url;
+                            <?php endif; ?>
+                        },
+                         'No': function() {
+                            $(this).dialog('close');
+                        }
+                    }
+                });
+
+                 $info.dialog('open');
+            });
+
+        });    
+
+
+
+
+      </script>
+
+  <?php
+}
+
+function sh_cd_get_cache($key)
+{
+    $key = sh_cd_generate_cache_key($key);
+
+    return get_transient($key);
+}
+function sh_cd_set_cache($key, $data)
+{
+    $key = sh_cd_generate_cache_key($key);
+
+    set_transient($key, $data, SH_CD_CACHING_TIME);
+}
+function sh_cd_delete_cache($key)
+{
+    $key = sh_cd_generate_cache_key($key);
+
+    return delete_transient($key);
+}
+function sh_cd_generate_cache_key($key)
+{
+    return SH_CD_SHORTCODE . $key;  
+}
---
+++ includes/pages.php
@@ -0,0 +1,267 @@
+<?php
+
+defined('ABSPATH') or die('Jog on!');
+
+function sh_cd_user_defined_page() {
+	
+	wp_enqueue_style('sh-cd-style', plugins_url( 'css/sh-cd.css', __FILE__ ));
+
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<h1>' . SH_CD_PLUGIN_NAME . '</h1>';
+
+
+	$this_page = get_permalink();
+
+	$action = (isset($_GET['action']) ? $_GET['action'] : 'view-all');
+
+
+	if ($action == 'save')
+	{
+		$slug = (isset($_POST['slug']) ? $_POST['slug'] : '');
+		$value = (isset($_POST['value']) ? $_POST['value'] : '');
+		$id = ((isset($_POST['existing-id']) && is_numeric($_POST['existing-id'])) ? $_POST['existing-id'] : false);
+
+		if(sh_cd_save_shortcode($slug, $value, $id))
+		{
+			sh_cd_display_message('Shortcode has been saved'); 
+		}
+		else
+		{
+			sh_cd_display_message('There was an error saving your shortcode', true);
+	 	}
+	}
+	elseif($action == 'delete')
+	{
+		if(sh_cd_delete_shortcode($_GET['id']))
+		{
+			sh_cd_display_message('Shortcode has been deleted!'); 
+		}
+		else
+		{
+			sh_cd_display_message('There was an error deleting your shortcode', true);
+	 	}	
+	}	
+
+	?>
+
+	<div class="wrap">
+
+	<div id="icon-options-general" class="icon32"></div>
+	
+	<div id="poststuff">
+
+		<div id="post-body" class="metabox-holder columns-3">
+
+			<!-- main content -->
+			<div id="post-body-content">
+
+				<div class="meta-box-sortables ui-sortable">
+					
+					<?php 
+
+					if ($action == 'add' || $action == 'edit')
+					{
+					
+
+						$title = 'Add Shortcode';
+						$slug = '';
+						$value = '';
+						$button_text = 'Add';
+						$existing_id = false;
+
+						if ($action == 'edit')
+						{
+							$title = 'Edit Shortcode';
+							$button_text = 'Save';
+							$existing_id = $_GET['id'];
+
+							// Get Shortcode from DB
+							$shortcode = sh_cd_get_shortcode($existing_id);
+							$slug = $shortcode->slug;
+							$value = stripslashes($shortcode->data);
+						}
+
+						$settings = array( 'textarea_name' => 'value' );
+						
+
+						?>
+						<a class="button-secondary" href="<?php echo admin_url('admin.php?page=sh-cd-shortcode-variables-user-defined'); ?>"><?php esc_attr_e( 'Cancel' ); ?></a>
+						<br /><br />
+						<div class="postbox">
+							<h3 class="hndle"><span><?php echo _e($title); ?> </span></h3>
+							<div style="padding: 0px 15px 0px 15px">
+								
+								<form method="post" action="<?php echo admin_url('admin.php?page=sh-cd-shortcode-variables-user-defined&action=save'); ?>">
+									<input type="hidden" id="existing-id" name="existing-id" value="<?php echo ((!empty($existing_id)) ? $existing_id : ''); ?>" />
+									<p>Slug:</p>
+									<input type="text" class="regular-text" size="100" id="slug" name="slug" <?php echo (('edit' == $action) ? ' disabled' : ''); ?> placeholder="<?php echo __('Slug'); ?>" <?php echo ((!empty($slug)) ? "value=\"$slug\"" : ""); ?>/>
+									<?php if ('edit' == $action): ?>
+										<p><small><?php echo __('Note: You can not edit a slug name. Editing a slug name may cause issues throughout your site. Please delete this shortcode and create another.'); ?></small></p>
+									<?php endif; ?>
+									<p>Shortcode data / Value:</p>
+									<!--<textarea id="value" name="value" cols="80" rows="10" class="large-text"><?php echo $value; ?></textarea><br>-->
+									<?php wp_editor( $value, 'value', $settings ); ?>
+									
+									<?php echo submit_button( $button_text . __(' Shortcode') ); ?>
+									
+								</form>
+							</div>
+						</div>
+
+					<?php
+					}
+					else
+					{?>
+						<a class="button-primary" href="<?php echo admin_url('admin.php?page=sh-cd-shortcode-variables-user-defined&action=add'); ?>"><?php esc_attr_e( 'Add a new Shortcode' ); ?></a>
+						<br /><br />
+					
+						
+								<div class="postbox">
+									<h3 class="hndle"><span><?php _e( 'Existing Shortcodes' ); ?> </span></h3>
+									<div style="padding: 0px 15px 0px 15px">
+										<br />
+										<table class="widefat" width="100%">
+											<tr class="row-title">
+												<th class="row-title" width="15%"><?php echo __('Slug'); ?></th>
+												<th width="20%"><?php echo __('Shortcode to embed'); ?></th>
+												<th width="*"><?php echo __('Shortcode Value'); ?></th>
+												<th width="15%"><?php echo __('Options'); ?></th>
+											</tr>
+											<?php
+
+											$current_shortcodes = sh_cd_get_all_shortcodes();
+
+											if ($current_shortcodes)
+											{
+												
+												$class = '';
+											
+												foreach ($current_shortcodes as $shortcode): 
+
+													$class = ($class == 'alternate') ? '' : 'alternate';
+
+													$edit_link = admin_url('admin.php?page=sh-cd-shortcode-variables-user-defined&action=edit&id=' . $shortcode->id);
+													$delete_link = admin_url('admin.php?page=sh-cd-shortcode-variables-user-defined&action=delete&id=' . $shortcode->id);
+											?>
+												<tr class="<?php echo $class; ?>">
+													<td><a href="<?php echo $edit_link; ?>"><?php echo $shortcode->slug; ?></a></td>
+													<td>[<?php echo SH_CD_SHORTCODE; ?> slug="<?php echo $shortcode->slug; ?>"]</td>
+													<td><textarea class="large-text"><?php echo esc_html(stripslashes($shortcode->data)); ?></textarea></td>
+													<td>
+														<a class="button button-small" href="<?php echo $edit_link; ?>"><?php echo __('Edit'); ?></a>
+														<a class="button button-small" href="<?php echo $delete_link; ?>" class="remove-confirm" ><?php echo __('Delete'); ?></a>
+													</td>
+												</tr>
+											<?php endforeach; 
+
+											}
+											else
+											{?>
+												<tr>
+													<td colspan="4" align="center"><?php echo __('You haven\'t created any shortcodes yet. <a href="' . admin_url('options-general.php?page=sh-cd-admin-page&action=add') . '">Add one now!</a>'); ?></td>
+												</tr>	
+											<?php
+											}
+											?>
+										</table>
+										<br />
+									</div>
+								</div>
+						<?php
+							
+															
+						
+					}
+?>
+					
+
+				</div>
+				<!-- .meta-box-sortables .ui-sortable -->
+
+			</div>
+			<!-- post-body-content -->
+
+		</div>
+		<!-- #post-body .metabox-holder .columns-2 -->
+
+		<br class="clear">
+	</div>
+	<!-- #poststuff -->
+
+</div> <?php
+
+	sh_cd_create_dialog_jquery_code(__('Are you sure?'), __('Are you sure you wish to delete this shortcode?'), 'remove-confirm');
+
+}
+
+
+function sh_cd_premade_shortcodes_page() {
+	
+		?>
+
+	<div class="wrap">
+
+	<div id="icon-options-general" class="icon32"></div>
+	
+	<div id="poststuff">
+
+		<div id="post-body" class="metabox-holder columns-3">
+
+			<!-- main content -->
+			<div id="post-body-content">
+
+				<div class="meta-box-sortables ui-sortable">
+							<div class="postbox">
+									<h3 class="hndle"><span><?php _e( 'Pre-made Shortcode Variables' ); ?> </span></h3>
+									<div style="padding: 0px 15px 0px 15px">
+										<p><?php echo __('Below is a list of premade shortcode variables that you can use throughout your website.'); ?></p>
+										<br />
+										<table class="widefat" width="100%">
+											<tr class="row-title">
+												<th class="row-title" width="30%"><?php echo __('Shortcode to embed'); ?></th>
+												<th width="*"><?php echo __('Description'); ?></th>
+												
+											</tr>
+											<?php
+
+											$premade_shortcodes = sh_cd_shortcode_presets();
+
+												$class = '';
+
+												foreach ($premade_shortcodes as $key => $description): 
+
+													$class = ($class == 'alternate') ? '' : 'alternate';
+
+													$shortcode = '[' . SH_CD_SHORTCODE. ' slug="' . $key . '"]';
+
+												?>
+												<tr class="<?php echo $class; ?>">
+													<td><?php echo $shortcode; ?></td>
+													<td><?php echo $description; ?></td>
+												</tr>
+											<?php endforeach; ?>
+										</table>
+										<br />
+										<p><?php echo __('<strong> Suggestion?</strong> Got an idea for a premade tag? If so, email me at: ') . '<a href="mailto:email@yeken.uk">email@yeken.uk</a>'; ?> </p>
+									</div>
+								</div>
+					
+
+				</div>
+				<!-- .meta-box-sortables .ui-sortable -->
+
+			</div>
+			<!-- post-body-content -->
+
+		</div>
+		<!-- #post-body .metabox-holder .columns-2 -->
+
+		<br class="clear">
+	</div>
+	<!-- #poststuff -->
+
+</div> <?php
+
+}
